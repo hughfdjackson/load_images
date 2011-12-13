2466a004595c8364775331dfd3747c86eb9b2030
hughfdjackson <hughfdjackson@googlemail.com>
2011-12-13 18:13:30 +0000

    added all

---
+++ src/load_images.js
@@ -0,0 +1,22 @@
+function load_images(image_object, callback){
+    var images = {},
+        to_load = 0
+
+    function on_load(){
+        to_load -= 1
+        if ( to_load == 0 && callback ) callback(images)
+    }
+
+    function load(key, val){
+        var image = new Image()
+        to_load += 1
+        image.src = val
+        image.onload = on_load
+        images[key] = image
+    }
+
+    for ( var prop in image_object ){
+        load(prop, image_object[prop])   
+    }
+
+}
