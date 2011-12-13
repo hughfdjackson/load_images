083d1fea755cc43795efa9f7f5ea3767cd9c0b73
hughfdjackson <hughfdjackson@googlemail.com>
2011-12-13 19:08:22 +0000

    i suspect i'm too sleepy for this

--- src-test/spec/load_images_spec.js
+++ src-test/spec/load_images_spec.js
@@ -16,8 +16,8 @@ describe("load_images", function(){
         }, "images to load", 5000)
         
         runs(function(){
-            expect(images.person.tagName.toLowerCase).equals('img')
-            expect(images.ninja.tagName.toLowerCase).equals('img')
+            expect(images.person.tagName.toLowerCase).toEqual('img')
+            expect(images.ninja.tagName.toLowerCase).toEqual('img')
         })
         
     })
