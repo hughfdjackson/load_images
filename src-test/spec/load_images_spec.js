677a90ef6be0b870f1d251e8828e7a8746a4d911
hughfdjackson <hughfdjackson@googlemail.com>
2011-12-13 18:39:31 +0000

    added unit tests

---
+++ src-test/spec/load_images_spec.js
@@ -0,0 +1,25 @@
+describe("load_images", function(){
+
+    it("should load images (duh)", function(){
+        var to_load = {
+                'person': '../images/person.png', 
+                'ninja': '../images/ninja.png'
+            },
+            images = {}
+
+        load_images(to_load, function(i){
+            images = i
+        })
+
+        waitsFor(function(){
+            return images.person && images.ninja            
+        }, "images to load", 5000)
+        
+        runs(function(){
+            expect(images.person instanceof Image).toBeTruthy()
+            expect(images.ninja instanceof Image).toBeTruthy()
+        })
+        
+    })
+
+})
